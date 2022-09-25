fd77293bb6f1619e9a94a1fdbbc1d439bad1ad33
Martin Tale <me@martintale.com>
2022-09-25 22:01:48 +0100

    fixed PWA

--- src/index.ts
+++ src/index.ts
@@ -214,7 +214,7 @@ window.addEventListener('beforeunload', function () {
 
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', () => {
-		navigator.serviceWorker.register('/service-worker.js').then(registration => {
+		navigator.serviceWorker.register('./service-worker.js').then(registration => {
 			console.log('SW registered: ', registration);
 		}).catch(registrationError => {
 			console.log('SW registration failed: ', registrationError);
