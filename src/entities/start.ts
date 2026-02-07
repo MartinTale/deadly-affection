b31d3b2c676d2a4bda3fa8eedea20c71cfa7c16f
Martin Tale <me@martintale.com>
2026-02-07 22:08:33 +0000

    fix

--- src/entities/start.ts
+++ src/entities/start.ts
@@ -1,17 +1,13 @@
-import { Rotation } from './../data/levels';
-import { el } from 'redom';
-import { SVGs } from '../helpers/svgs';
-import { getSVGElement } from '../helpers/utilities';
-import { state } from '../systems/state';
-import { colorSchemes } from '../systems/game';
-import { Base, PathDirection } from './base';
+import { Rotation } from "./../data/levels";
+import { el } from "redom";
+import { SVGs } from "../helpers/svgs";
+import { getSVGElement } from "../helpers/utilities";
+import { state } from "../systems/state";
+import { colorSchemes } from "../systems/game";
+import { Base, PathDirection } from "./base";
 
 export class Start extends Base {
-	constructor(
-		rotation: number,
-		cellKey: string,
-		outputs?: (Rotation | null)[],
-	) {
+	constructor(rotation: number, cellKey: string, outputs?: (Rotation | null)[]) {
 		const scheme = colorSchemes[state.colorScheme];
 		const iconSvg = getSVGElement(SVGs[scheme.icon]);
 
@@ -25,9 +21,9 @@ export class Start extends Base {
 		}
 
 		super(
-			'start',
+			"start",
 			cellKey,
-			el('div.cell'),
+			el("div.cell"),
 			iconSvg,
 			rotation,
 			true,
